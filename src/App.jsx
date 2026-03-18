69c0b1b4f727529c55e9a27bae6a7ee46dc393ff
abusayed2050 <abu.sayed2050@gmail.com>
2026-03-18 15:59:17 +0600

    prepare a React project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='border-red-700 border '>
+          <h1>Vite + React</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
